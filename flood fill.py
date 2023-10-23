3d4aa7959d6c37eadd353c92929a4838120851a6
Kate <94894514+katesheet@users.noreply.github.com>
2023-10-23 00:48:43 -0700

    Add files via upload

---
+++ flood fill.py
@@ -0,0 +1,60 @@
+from typing import List
+
+board = [
+    "......................",
+    "......##########......",
+    "......#........#......",
+    "......#........#......",
+    "......#........#####..",
+    "....###............#..",
+    "....#............###..",
+    "....##############....",
+]
+
+
+def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
+    """
+    Args:
+        input_board (List[str])
+        old (str): Value to be replaced
+        new (str): Value that replaces the old
+        x (int): X-coordinate of the flood start point
+        y (int): Y-coordinate of the flood start point
+    Returns:
+        List[str]: Modified board
+    """
+
+    # Implement your code here.
+
+    # print(input_board[x])
+    # print(type(input_board[x]))
+    # print(input_board[x][y])
+    m = len(input_board)
+    n = len(input_board[0])
+
+    if x >= 0 and x < m and y >= 0 and y < n:
+        if input_board[x][y] == old:
+            # input_board[x][y] = new
+            # print(input_board)
+            input_board[x] =  input_board[x][:y]+ new + input_board[x][y+1:]
+            flood_fill(input_board = input_board, old = old, new = new, x = x-1, y = y)
+            flood_fill(input_board = input_board, old = old, new = new, x = x+1, y = y)
+            flood_fill(input_board = input_board, old = old, new = new, x = x, y = y-1)
+            flood_fill(input_board = input_board, old = old, new = new, x = x, y = y+1)
+    return input_board
+
+
+modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
+
+for a in modified_board:
+    print(a)
+
+# Expected output:
+# ......................
+# ......##########......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#......
+# ......#~~~~~~~~#####..
+# ....###~~~~~~~~~~~~#..
+# ....#~~~~~~~~~~~~###..
+# ....##############....
